0de674a64a7ab10cd56cbfc8139cecb33e530c68
Nicolas BONNIOT <nicolas@devgiants.fr>
2019-08-05 14:50:03 +0200

    Initiate minimal structure

---
+++ LchMaintenanceBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nicolas
+ * Date: 05/08/19
+ * Time: 14:41
+ */
+
+namespace Lch\MaintenanceBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LchMaintenanceBundle extends Bundle
+{
+
+}
