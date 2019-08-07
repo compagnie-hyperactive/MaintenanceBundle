e59b6d433b16384816f30db68f6c4386821b88b9
Nicolas BONNIOT <nicolas@devgiants.fr>
2019-08-07 16:59:05 +0200

    Introduce tests

--- Command/ToggleMaintenanceCommand.php
+++ Command/ToggleMaintenanceCommand.php
@@ -3,11 +3,9 @@
 namespace Lch\MaintenanceBundle\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Exception\InvalidOptionException;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
 /**
---
+++ Tests/MaintenanceModeTest.php
@@ -0,0 +1,104 @@
+<?php
+
+
+namespace Lch\MaintenanceBundle\Tests\Command;
+
+
+use Lch\MaintenanceBundle\Command\ToggleMaintenanceCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Tester\CommandTester;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+
+class MaintenanceModeTest extends WebTestCase
+{
+
+    /** @var Command */
+    protected $maintenanceCommand;
+
+    /** @var CommandTester */
+    protected $commandTester;
+
+    /** @var string */
+    protected $exposedDir;
+
+    /** @var Filesystem */
+    protected $filesystem;
+
+    /** @var Client */
+    protected $client;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        static::bootKernel();
+        $this->exposedDir         = static::$kernel
+                                        ->getContainer()
+                                        ->getParameter('kernel.project_dir') . DIRECTORY_SEPARATOR . 'public';
+        $application              = new Application(static::$kernel);
+        $this->maintenanceCommand = $application->find(ToggleMaintenanceCommand::$defaultName);
+        $this->commandTester      = new CommandTester($this->maintenanceCommand);
+        $this->filesystem         = new Filesystem();
+        $this->client             = static::createClient();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function tearDown()
+    {
+        $this->exposedDir         = null;
+        $this->maintenanceCommand = null;
+        $this->commandTester      = null;
+        $this->filesystem         = null;
+    }
+
+    /**
+     * Check maintenance flag proper addition
+     */
+    public function testMaintenanceSet()
+    {
+        $this->commandTester->execute(
+            [
+                'command'                                       => $this->maintenanceCommand->getName(),
+                ToggleMaintenanceCommand::MAINTENANCE_PARAMETER => 1,
+            ]
+        );
+
+        $this->assertTrue(
+            $this
+                ->filesystem
+                ->exists($this->exposedDir . DIRECTORY_SEPARATOR . ToggleMaintenanceCommand::FILE_NAME)
+        );
+
+        $this->client->request('GET', '/');
+        $this->assertEquals(Response::HTTP_SERVICE_UNAVAILABLE, $this->client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * Check maintenance flag proper deletion
+     */
+    public function testMaintenanceUnset()
+    {
+        $this->commandTester->execute(
+            [
+                'command'                                       => $this->maintenanceCommand->getName(),
+                ToggleMaintenanceCommand::MAINTENANCE_PARAMETER => 0,
+            ]
+        );
+
+        $this->assertFalse(
+            $this
+                ->filesystem
+                ->exists($this->exposedDir . DIRECTORY_SEPARATOR . ToggleMaintenanceCommand::FILE_NAME)
+        );
+
+        $this->client->request('GET', '/');
+        $this->assertEquals(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+}
